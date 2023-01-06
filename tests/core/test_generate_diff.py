31b0c5b1795bc5b21d45a0922a5940893ad1a62f
Andrey Zhelezov <azhelezov@mail.ru>
2023-01-06 22:58:05 +0300

    tests: Add yaml files tests.

--- tests/core/test_generate_diff.py
+++ tests/core/test_generate_diff.py
@@ -9,9 +9,17 @@ def cwd():
 
 
 def test_generate_diff(cwd):
-    print(__file__)
     with open(f'{cwd}/../fixtures/test_generate_diff/result')as f:
         result = f.read()
+
     f1_path = f'{cwd}/../fixtures/test_generate_diff/file1.json'
     f2_path = f'{cwd}/../fixtures/test_generate_diff/file2.json'
+    assert generate_diff(f1_path, f2_path) == result
+
+    f1_path = f'{cwd}/../fixtures/test_generate_diff/file1.yaml'
+    f2_path = f'{cwd}/../fixtures/test_generate_diff/file2.yaml'
+    assert generate_diff(f1_path, f2_path) == result
+
+    f1_path = f'{cwd}/../fixtures/test_generate_diff/file1.yml'
+    f2_path = f'{cwd}/../fixtures/test_generate_diff/file2.yml'
     assert generate_diff(f1_path, f2_path) == result
