293596cdb61d113c8954ae51f902e2e828754348
Andrey Zhelezov <azhelezov@mail.ru>
2023-02-11 15:42:21 +0300

    feat: Add format check in script.

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -3,12 +3,19 @@ from gendiff import generate_diff
 
 
 def main():
+    output_formats = ['stylish', 'plain', 'json']
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output. Formats: [stylish|plain]', default='stylish')
+    parser.add_argument('-f', '--format',
+                        help=f'set format of output. Formats: [{"|".join(output_formats)}]',
+                        default='stylish')
     args = parser.parse_args()
+    if args.format not in output_formats:
+        print(f'Format "{args.format}" is not available!')
+        parser.print_help()
+        exit(1)
 
     file1_path = args.first_file
     file2_path = args.second_file
